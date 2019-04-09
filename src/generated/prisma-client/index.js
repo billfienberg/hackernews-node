4621d1b9b498df8a8a53363376188d05da10bbd1
Bill Fienberg <billfienberg@gmail.com>
2019-04-09 07:17:37 -0500

    Finish step 4

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Link",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://us1.prisma.sh/bill-fienberg-88b03b/hackernews-node/dev`
+});
+exports.prisma = new exports.Prisma();
