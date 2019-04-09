5aac407276286c261ae8b5830a8a9b9adf215954
Bill Fienberg <billfienberg@gmail.com>
2019-04-09 07:38:30 -0500

    Finish steps 5 and 6

--- src/index.js
+++ src/index.js
@@ -1,37 +1,25 @@
 const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
 
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  }
-];
-let idCount = links.length;
-
-// 2
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links
-  },
-  Mutation: {
-    // 2
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
-    }
-  }
+  Query,
+  Mutation,
+  User,
+  Link
 };
 
-// 3
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: request => {
+    return {
+      ...request,
+      prisma
+    };
+  }
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
