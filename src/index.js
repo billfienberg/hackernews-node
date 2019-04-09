55f9a6749e24d9c76e714e2a03a9a6838caf122b
Bill Fienberg <billfienberg@gmail.com>
2019-04-09 06:55:23 -0500

    Finish step 3

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Mutation {
-  post(url: String!, description: String!): Link!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`;
-
 let links = [
   {
     id: "link-0",
@@ -25,6 +7,7 @@ let links = [
     description: "Fullstack tutorial for GraphQL"
   }
 ];
+let idCount = links.length;
 
 // 2
 const resolvers = {
@@ -32,16 +15,23 @@ const resolvers = {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links
   },
-  Link: {
-    id: parent => parent.id,
-    description: parent => parent.description,
-    url: parent => parent.url
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    }
   }
 };
 
 // 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
